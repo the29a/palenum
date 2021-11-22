cf07821d5d1454dcdb98f7b0fc48792d1e787c71
the29a <deathlike333@gmail.com>
2021-11-22 19:47:16 +0300

    Add some parts

--- README.md
+++ README.md
@@ -3,10 +3,19 @@ Pale Privilege Escalation Emumeration script
 
 palenum - light shell script, able to file output.
 
+Get:
+- Systeminfo
+- crontab
+- network 
+- mount points
+- sticky bit, GUID and SUID
+- enumerating users and env info
+- ssh keys
+
 ## To-Do:    
 [ ] - Make everything ok 
 
 ## Reference links:    
-[LinEnum](https://github.com/rebootuser/LinEnum)
-[LinPEAS](https://github.com/carlospolop/PEASS-ng/tree/master/linPEAS)
-[PayloadAllTheThings](https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/Methodology%20and%20Resources/Linux%20-%20Privilege%20Escalation.md)
+[LinEnum](https://github.com/rebootuser/LinEnum)    
+[LinPEAS](https://github.com/carlospolop/PEASS-ng/tree/master/linPEAS)    
+[PayloadAllTheThings](https://github.com/swisskyrepo/PayloadsAllTheThings/blob/master/Methodology%20and%20Resources/Linux%20-%20Privilege%20Escalation.md)    
--- palemum.sh
+++ palemum.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 #check root
-
 is_user_root () { [ "$(id -u)" -eq 0 ]; }
 
 if is_user_root; then
@@ -14,28 +13,41 @@ fi
 # Who i am
 echo "\nWho i am:"
 id
-
-# Who online?
 echo "\nAnybody here?"
 w
 
 # System
 ##  Kernel version
 echo "\nKernel version:"
-uname -a
+cat /proc/version
 ## Check distro
 echo "\nCheck distro:"
 cat /etc/issue
 cat /etc/*-release
+echo "Hostname is:" ; hostname
 
-## Crontab
+# Crontab
 echo "\nCheck crontab:"
 crontab -l
+echo "\nScheduled cron jobs"
+ls -la /etc/cron* 2>/dev/null
+echo "\nWritable cron dirs"
+
 
 # Network
-echo "Hostname is:" ; hostname
 echo "\nIp:"
 ip -br a show
+echo "\nShow routes:"
+route
+echo "\nNetstat:"
+netstat -antup
+
+# File system
+echo "\nShow Mounts:"
+mount
+echo "\nShow fstab"
+cat /etc/fstab 2>/dev/null
+
 
 # #https://gtfobins.github.io/
 #Get sticky bit
@@ -46,10 +58,29 @@ find / -perm -u=s -type f 2>/dev/null
 echo "\nGet GUID:"
 find / -perm -g=s -type f 2>/dev/null 
 
+# Enumerating users and env info
+echo "\nCurrent User:" 
+whoami
+echo "\nCurrent User ID:"
+"id" 
+echo "\nAll users:"
+cat /etc/passwd 
+echo "\nSuper Users Found:"
+grep -v -E '^#' /etc/passwd | awk -F: '$3 == 0{print $1}' 
+echo "\nEnvironment:"
+env 2>/dev/null | grep -v 'LS_COLORS' 
+echo "\nSudoers (privileged):"
+cat /etc/sudoers 2>/dev/null | grep -v '#' 2>/dev/null
+echo "\nList out any screens running for the current user:" 
+screen -ls 2>/dev/null  
+echo "\nLogged in User Activity:" 
+who -a 2>/dev/null
+
 # Search ssh keys
 echo "\nSSH keys"
 find / -name authorized_keys 2> /dev/null
 find / -name id_rsa 2> /dev/null
 
-# Looking for passwords, make a shitfull wall of text, may me add later
+echo "\nDone."
+# Looking for passwords, make a shitfull wall of text, may be add later
 # grep --color=auto -rnw '/' -ie "PASSWORD" --color=always 2> /dev/null
